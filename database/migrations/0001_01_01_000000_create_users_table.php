70b0f72f8f94ec95e6cd9dfbd41002ba0459437a
DaviGadelhaQA <qa.davigadelha@gmail.com>
2026-04-01 19:50:09 -0300

    Update role field on users table migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->date('birth_date');
-            $table->string('role');
+            $table->string('role', 50);
             $table->string('password');
             $table->timestamps();
         });
